7dd68851b9f45977979f16c4ea5f56575614a3f5
pebelinSystem <pebelin.hernandez@pegasoconsulting.net>
2025-04-27 18:07:14 -0400

    cambiando a usar hash en router

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 // src/router.js
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory  } from 'vue-router';
+
 import HelloWorld from './components/HelloWorld.vue'; // Asegúrate de crear este componente
 import MyComponent from './components/my-component.vue'; // Asegúrate de crear este componente
 
@@ -23,7 +24,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory (),
   routes
 });
 
